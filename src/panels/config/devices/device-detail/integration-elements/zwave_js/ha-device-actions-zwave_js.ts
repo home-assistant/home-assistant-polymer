3ec44117e0c13d9f1f48c816dd2243cfaf965a78
Charles Garwood <cgarwood@gmail.com>
2021-07-08 20:07:49 -0400

    Initial commit for Remove Failed Node UI

--- src/panels/config/devices/device-detail/integration-elements/zwave_js/ha-device-actions-zwave_js.ts
+++ src/panels/config/devices/device-detail/integration-elements/zwave_js/ha-device-actions-zwave_js.ts
@@ -16,6 +16,7 @@ import {
 import { haStyle } from "../../../../../../resources/styles";
 import { HomeAssistant } from "../../../../../../types";
 import { showZWaveJSReinterviewNodeDialog } from "../../../../integrations/integration-panels/zwave_js/show-dialog-zwave_js-reinterview-node";
+import { showZWaveJSRemoveFailedNodeDialog } from "../../../../integrations/integration-panels/zwave_js/show-dialog-zwave_js-remove-failed-node";
 
 @customElement("ha-device-actions-zwave_js")
 export class HaDeviceActionsZWaveJS extends LitElement {
@@ -51,10 +52,18 @@ export class HaDeviceActionsZWaveJS extends LitElement {
           )}
         </mwc-button>
       </a>
+<<<<<<< HEAD
       <mwc-button @click=${this._reinterviewClicked}>
         ${this.hass.localize(
           "ui.panel.config.zwave_js.device_info.reinterview_device"
         )}
+=======
+      <mwc-button @click=${this._reinterviewClicked}
+        >Re-interview Device</mwc-button
+      >
+      <mwc-button @click=${this._removeFailedNode}>
+        Remove Failed Device
+>>>>>>> Initial commit for Remove Failed Node UI
       </mwc-button>
     `;
   }
@@ -64,6 +73,16 @@ export class HaDeviceActionsZWaveJS extends LitElement {
       return;
     }
     showZWaveJSReinterviewNodeDialog(this, {
+      entry_id: this._entryId,
+      node_id: this._nodeId,
+    });
+  }
+
+  private async _removeFailedNode() {
+    if (!this._nodeId || !this._entryId) {
+      return;
+    }
+    showZWaveJSRemoveFailedNodeDialog(this, {
       entry_id: this._entryId,
       node_id: this._nodeId,
     });
